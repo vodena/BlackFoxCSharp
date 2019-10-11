5532b4165fbb181102cb5a863dfbe0a545059b08
paulidealiste <paulidealiste@aol.com>
2019-10-11 14:47:20 +0200

    Empty constructor

--- Model/ConvergencyCriterion.cs
+++ Model/ConvergencyCriterion.cs
@@ -26,7 +26,7 @@ namespace BlackFoxCSharp.Model
     /// Configuration for optimization early stopping
     /// </summary>
     [DataContract]
-    public partial class ConvergencyCriterion :  IEquatable<ConvergencyCriterion>
+    public partial class ConvergencyCriterion : IEquatable<ConvergencyCriterion>
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="ConvergencyCriterion" /> class.
@@ -38,17 +38,18 @@ namespace BlackFoxCSharp.Model
             this.NumberOfLatestGenerations = numberOfLatestGenerations;
             this.PercentageOfTolerance = percentageOfTolerance;
         }
-        
+        public ConvergencyCriterion() { }
+
         /// <summary>
         /// Gets or Sets NumberOfLatestGenerations
         /// </summary>
-        [DataMember(Name="numberOfLatestGenerations", EmitDefaultValue=false)]
+        [DataMember(Name = "numberOfLatestGenerations", EmitDefaultValue = false)]
         public int? NumberOfLatestGenerations { get; set; }
 
         /// <summary>
         /// Gets or Sets PercentageOfTolerance
         /// </summary>
-        [DataMember(Name="percentageOfTolerance", EmitDefaultValue=false)]
+        [DataMember(Name = "percentageOfTolerance", EmitDefaultValue = false)]
         public double? PercentageOfTolerance { get; set; }
 
         /// <summary>
@@ -64,7 +65,7 @@ namespace BlackFoxCSharp.Model
             sb.Append("}\n");
             return sb.ToString();
         }
-  
+
         /// <summary>
         /// Returns the JSON string presentation of the object
         /// </summary>
@@ -94,12 +95,12 @@ namespace BlackFoxCSharp.Model
             if (input == null)
                 return false;
 
-            return 
+            return
                 (
                     this.NumberOfLatestGenerations == input.NumberOfLatestGenerations ||
                     (this.NumberOfLatestGenerations != null &&
                     this.NumberOfLatestGenerations.Equals(input.NumberOfLatestGenerations))
-                ) && 
+                ) &&
                 (
                     this.PercentageOfTolerance == input.PercentageOfTolerance ||
                     (this.PercentageOfTolerance != null &&
